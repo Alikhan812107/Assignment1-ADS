60969a63337d2e6b363f7df2f52f8da93ae940ef
Nitro5 <amd812107@gmail.com>
2025-03-29 01:40:17 +0500

    812107

--- src/Task1.java
+++ src/Task1.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Task1 {
+    public static int findMinimum(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(findMinimum(arr));
+        scanner.close();
+    }
 }
