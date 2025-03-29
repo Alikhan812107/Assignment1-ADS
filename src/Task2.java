aa52c58a0a697f729bcee3c9ce67382a753767ef
Nitro5 <amd812107@gmail.com>
2025-03-29 12:42:36 +0500

    812107fwa

--- src/Task2.java
+++ src/Task2.java
@@ -1,5 +1,7 @@
+// importing scanner
 import java.util.Scanner;
 
+// function which finds an average
 public class Task2 {
     public static double findAverage(int[] arr) {
         int sum = 0;
@@ -9,6 +11,7 @@ public class Task2 {
         return (double) sum / arr.length;
     }
 
+    //main function which reads user input and runs a function, processes the result and closing a scanner
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
