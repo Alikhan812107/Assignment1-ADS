841dd19ed76043c669acedaabde8830358f4dd76
Nitro5 <amd812107@gmail.com>
2025-04-01 01:48:38 +0500

    812107fwa

---
+++ src/Task4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * This function based on mathematical approach which we usually use when identifying factorial
+ * it also uses recursive approach along with math.
+ * for example 4! is 4 * 3! which is 3 * 2! which is 2 * 1! which is 1 * 0! which is the base case??? or final point???
+ * Time complexity is O(n)
+ *
+ **/
+
+public class Task4 {
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    // just a main function
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(factorial(n));
+        scanner.close();
+    }
+}
